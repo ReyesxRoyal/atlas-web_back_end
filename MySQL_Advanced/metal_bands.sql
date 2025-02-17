36761e3f2d74da446d670e6b07ebcc71ba2d7e1b
ReyesxRoyal <rasheed.r.white@gmail.com>
2025-02-16 23:09:02 -0600

    .

--- MySQL_Advanced/metal_bands.sql
+++ MySQL_Advanced/metal_bands.sql
@@ -14,7 +14,7 @@ CREATE TABLE `metal_bands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `band_name` varchar(255) DEFAULT NULL,
   `fans` int(11) DEFAULT NULL,
-  `formed` Year DEFAULT NULL,
+  `formed` year DEFAULT NULL,
   `origin` varchar(255) DEFAULT NULL,
   `split` year DEFAULT NULL,
   `style` varchar(255) DEFAULT NULL,
