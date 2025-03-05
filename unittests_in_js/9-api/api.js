979e29176bdebb53bf3c5b743eead85f4bf0d7ab
ReyesxRoyal <rasheed.r.white@gmail.com>
2025-03-05 01:21:35 -0600

    .

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+    const id = req.params.id;
+    if (!isNaN(id)) {
+        res.send(`Payment methods for cart ${id}`);
+    } else {
+        res.status(404).send('Cart not found');
+    }
+});
+
+app.listen(port, () => {
+    console.log('API available on localhost port 7865');
+});
